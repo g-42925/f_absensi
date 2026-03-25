84db3c3114c2cea14d61cc1a2179a4029a6a35de
g-42925 <ecipersia@gmail.com>
2026-03-25 09:53:14 +0700

    feat: improve location permission handling by requesting permission and add a timeout for current position retrieval.

--- lib/providers/location_provider.dart
+++ lib/providers/location_provider.dart
@@ -1,36 +1,38 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:geolocator/geolocator.dart';
-
 final locationProvider = FutureProvider<Position>((ref) async {
+  // 1. cek GPS
   bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-  if (!serviceEnabled) throw Exception('GPS is not enabled');
-
-  LocationPermission permission = await Geolocator.checkPermission();
-
   if (!serviceEnabled) throw Exception('GPS is not enabled');
   
-  if (permission == LocationPermission.denied) {
-    throw Exception('Permission denied');
-  }
 
-  if (permission == LocationPermission.deniedForever) {
-    throw Exception('Location permission denied forever');
-  }
+  // 2. cek permission
+  LocationPermission permission = await Geolocator.checkPermission();
 
+  if (permission == LocationPermission.denied) permission = await Geolocator.requestPermission(); 
+
+  if (permission == LocationPermission.deniedForever) throw Exception('Location permission denied forever');
+
+  if (permission == LocationPermission.denied) throw Exception('Permission denied');
+  
+
+  
+
+  // 3. retry
   int retry = 0;
 
   while (retry < 3) {
     try {
       return await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
+        timeLimit: const Duration(seconds: 5),
       );
-    } catch (e) {
+    } 
+    catch (e) {
       retry++;
       await Future.delayed(const Duration(seconds: 1));
     }
   }
 
+  // 4. fallback
   final last = await Geolocator.getLastKnownPosition();
 
   if (last != null) {
