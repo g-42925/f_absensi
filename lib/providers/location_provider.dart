782b4327f05b1c706feca984f730fb8513dd31bd
g-42925 <ecipersia@gmail.com>
2026-03-25 09:36:45 +0700

    feat: enhance location provider error handling and retry logic for position acquisition, and update API endpoint to local development URL.

--- lib/providers/location_provider.dart
+++ lib/providers/location_provider.dart
@@ -2,17 +2,40 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
 
 final locationProvider = FutureProvider<Position>((ref) async {
+  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+  if (!serviceEnabled) throw Exception('GPS is not enabled');
+
   LocationPermission permission = await Geolocator.checkPermission();
 
+  if (!serviceEnabled) throw Exception('GPS is not enabled');
+  
   if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
+    throw Exception('Permission denied');
   }
 
   if (permission == LocationPermission.deniedForever) {
     throw Exception('Location permission denied forever');
   }
 
-  return await Geolocator.getCurrentPosition(
-    desiredAccuracy: LocationAccuracy.high,
-  );
+  int retry = 0;
+
+  while (retry < 3) {
+    try {
+      return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+    } catch (e) {
+      retry++;
+      await Future.delayed(const Duration(seconds: 1));
+    }
+  }
+
+  final last = await Geolocator.getLastKnownPosition();
+
+  if (last != null) {
+    return last;
+  }
+
+  throw Exception('failed to get current location');
 });
